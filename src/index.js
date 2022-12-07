40d9cb9ae646261c9ca11eb5e6e3072160893ba8
Stu <stumac1000@yahoo.com>
2022-12-06 17:06:25 -0800

    Save often

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './normalize.css';
 import './style.css';
 import shed from './pictures/pexels-stein-egil-liland-5740848.jpg';
+// Import the other photos too?
+
 
 console.log('suck my teet! eat eat eat');
