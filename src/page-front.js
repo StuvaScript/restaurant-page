cf7a97d94360a6b202c60c8dc79c2d61099b879a
Stu <stumac1000@yahoo.com>
2022-12-15 13:33:02 -0800

    Finish making front page with clean code

--- src/page-front.js
+++ src/page-front.js
@@ -2,17 +2,11 @@ export default function frontPage() {
 
     const innerObject = {
         init: function() {
-
-            // Do I even need to cache the DOM here??
-            // this.cacheDom(); 
-
             this.createHeader();
+            this.createMain();
+            this.createFooter();
         },
-        cacheDom: function() {
-            // Do I even need this??
-            // this.content = document.querySelector('#content');
-        },
-        domManipulate: function(element, innerText, className, appendTo, ...attributes) {
+        manipulateDOM: function(element, innerText, className, appendTo, ...attributes) {
             let newElement = document.createElement(element);
             (innerText !== '') ? newElement.innerText = innerText : '';
             (className !== '') ? newElement.classList.add(className) : '';
@@ -29,15 +23,56 @@ export default function frontPage() {
             };
         },
         createHeader: function() {
-            
-            // element, innerText, className, appendTo, ...attributes
-
-            this.header = this.domManipulate('header', '', '', '#content');
-            this.headerH1 = this.domManipulate('h1', '', '', 'header');
-            this.headerSpan1 = this.domManipulate('span', 'Abandoned', '', 'h1');
-            this.headerBr = this.domManipulate('br', '', '', 'h1');
-            this.headerSpan2 = this.domManipulate('span', 'Shed Café', '', 'h1');
-            this.headerImg = this.domManipulate('img', '', 'shed-img', 'header', 'src', '../src/pictures/pexels-stein-egil-liland-5740848.jpg', 'alt', 'A rundown shed at night.');
+            this.header = this.manipulateDOM('header', '', '', '#content');
+            this.H1 = this.manipulateDOM('h1', '', '', 'header');
+            this.span = this.manipulateDOM('span', 'Abandoned', '', 'h1');
+            this.br = this.manipulateDOM('br', '', '', 'h1');
+            this.span = this.manipulateDOM('span', 'Shed Café', '', 'h1');
+            this.img = this.manipulateDOM('img', '', 'shed-img', 'header', 'src', '../src/pictures/pexels-stein-egil-liland-5740848.jpg', 'alt', 'A rundown shed at night.');
+        },
+        createMain: function() {            
+            this.main = this.manipulateDOM('main', '', '', '#content');
+            this.div = this.manipulateDOM('div', '', 'sidebar', 'main');
+            this.ul = this.manipulateDOM('ul', '', '', '.sidebar');
+            this.li = this.manipulateDOM('li', 'Who We Are', '', 'ul');
+            this.li = this.manipulateDOM('li', 'Our Creations', '', 'ul');
+            this.li = this.manipulateDOM('li', 'Reserve A Table', '', 'ul');
+            this.div = this.manipulateDOM('div', '', 'feature', 'main');
+            this.img = this.manipulateDOM('img', '', 'interior-img', '.feature', 'src', '../src/pictures/pexels-arthouse-studio-4344544.jpg', 'alt', 'Interior of restaurant.');
+            this.div = this.manipulateDOM('div', '', 'info', '.feature');
+            this.div = this.manipulateDOM('div', '', 'para-div', '.info');
+            this.p = this.manipulateDOM('p', this.longText.p1, '', '.para-div');
+            this.br = this.manipulateDOM('br', '', '', '.para-div');
+            this.p = this.manipulateDOM('p', this.longText.p2, '', '.para-div');
+            this.img = this.manipulateDOM('img', '', 'eating-img', '.feature', 'src', '../src/pictures/pexels-helena-lopes-696218.jpg', 'alt', 'People eating.');
+            this.img = this.manipulateDOM('img', '', 'cooking-img', '.feature', 'src', '../src/pictures/pexels-araz-yurtseven-13739759.jpg', 'alt', 'Cooking food over fire.');
+            this.img = this.manipulateDOM('img', '', 'fish-img', '.feature', 'src', '../src/pictures/pexels-алекке-блажин-11244934.jpg', 'alt', 'Fish head food.');
+            this.img = this.manipulateDOM('img', '', 'fire-img', 'main', 'src', '../src/pictures/johnathan-macedo-4NQEvxW2_4w-unsplash.jpg', 'alt', 'Chef cooking with fire.');
+        },
+        createFooter: function() {
+            this.footer = this.manipulateDOM('footer', '', '', '#content');
+            this.p = this.manipulateDOM('p', 'Credits', '', 'footer');
+            this.ul = this.manipulateDOM('ul', '', 'foot-list', 'footer');
+            this.li = this.manipulateDOM('li', '', 'fLi1', '.foot-list');
+            this.a = this.manipulateDOM('a', 'stein egil liland', '', '.fLi1', 'href', 'https://www.pexels.com/photo/abandoned-wooden-barn-on-grassy-terrain-against-starry-night-sky-5740848/', 'target', '_blank');
+            this.li = this.manipulateDOM('li', '', 'fLi2', '.foot-list');
+            this.a = this.manipulateDOM('a', 'ArtHouse Studio', '', '.fLi2', 'href', 'https://www.pexels.com/photo/wooden-interior-of-modern-restaurant-in-marine-style-4344544/', 'target', '_blank');
+            this.li = this.manipulateDOM('li', '', 'fLi3', '.foot-list');
+            this.a = this.manipulateDOM('a', 'Araz Yurtseven', '', '.fLi3', 'href', 'https://www.pexels.com/photo/a-person-cooking-in-a-pan-outdoors-13739759/', 'target', '_blank');
+            this.li = this.manipulateDOM('li', '', 'fLi4', '.foot-list');
+            this.a = this.manipulateDOM('a', 'Helena Lopes', '', '.fLi4', 'href', 'https://www.pexels.com/photo/people-drinking-liquor-and-talking-on-dining-table-close-up-photo-696218/', 'target', '_blank');
+            this.li = this.manipulateDOM('li', '', 'fLi5', '.foot-list');
+            this.a = this.manipulateDOM('a', 'Алекке Блажин', '', '.fLi5', 'href', 'https://www.pexels.com/photo/close-up-photograph-of-fish-heads-near-tomatoes-11244934/', 'target', '_blank');
+            this.li = this.manipulateDOM('li', '', 'fLi6', '.foot-list');
+            this.a = this.manipulateDOM('a', 'Johnathan Macedo', '', '.fLi6', 'href', 'https://unsplash.com/photos/4NQEvxW2_4w', 'target', '_blank');
+            this.li = this.manipulateDOM('li', '', 'fLi7', '.foot-list');
+            this.a = this.manipulateDOM('a', 'Good Ware', '', '.fLi7', 'href', 'https://www.flaticon.com/free-icon/fish-bone_4201633?term=fish%20bones&page=1&position=1&page=1&position=1&related_id=4201633&origin=style', 'title', 'fish bone icons', 'target', '_blank');
+            this.li = this.manipulateDOM('li', '', 'fLi8', '.foot-list');
+            this.a = this.manipulateDOM('a', 'Doggo Ipsum', '', '.fLi8', 'href', 'https://doggoipsum.com/', 'target', '_blank');
+        },
+        longText: {
+            p1: 'Doggo ipsum borkf puggo noodle horse heckin good boys long water shoob, heckin good boys and girls very jealous pupper very taste wow. Pupper boof aqua doggo lotsa pats very good spot pupper much ruin diet very good spot boof super chub, such treat borkdrive blop the neighborhood pupper yapper wrinkler shooberino. Pats woofer smol borking doggo with a long snoot for pats wrinkler lotsa pats most angery pupper I have ever seen, ur givin me a spook.',
+            p2: 'Bork h*ck heckin good boys and girls doing me a frighten doggorino adorable doggo, many pats aqua doggo doge pats clouds ruff, sub woofer lotsa pats very hand that feed shibe noodle horse. Fat boi wrinkler what a nice floof he made many woofs very taste wow, the neighborhood pupper long water shoob blop, noodle horse very taste wow very good spot. Blop you are doing me a frighten he made many woofs blep big ol tungg ruff very hand that feed.',
         },
     };
 
