281eec8794963779a88aca9ba1be8c2d664f9dec
Stu <stumac1000@yahoo.com>
2023-04-24 18:05:10 -0700

    Make a responsive design

--- src/index.js
+++ src/index.js
@@ -1,45 +1,46 @@
-import pageMain from './page-main.js';
-import pageAbout from './page-about.js';
-import pageFood from './page-food.js';
-import pageReserve from './page-reserve';
-import './normalize.css';
-import './style.css';
+import pageMain from "./page-main.js";
+import pageAbout from "./page-about.js";
+import pageFood from "./page-food.js";
+import pageReserve from "./page-reserve";
+import "./normalize.css";
+import "./style.css";
 
 pageMain();
 
 function component() {
+  // pageAbout();
+  pageFood();
 
-    pageAbout();
+  function removeElements() {
+    const picky = document.querySelectorAll("main > *:not(:first-child)");
+    for (const el of picky) {
+      el.remove();
+    }
+  }
 
-    function removeElements() {
-        const picky = document.querySelectorAll('main > *:not(:first-child)');
-        for (const el of picky) {
-            el.remove();
-        }
-    };
+  document
+    .querySelector(".sidebar > ul")
+    .addEventListener("click", function (e) {
+      const idx = [...this.children].indexOf(e.target);
 
-    document.querySelector('.sidebar > ul').addEventListener('click', function(e) {
-        const idx = [...this.children].indexOf(e.target);
-
-        switch (idx) {
-            case 0:
-                removeElements();
-                pageAbout();
-                break;
-            case 1:
-                removeElements();
-                pageFood();
-                break;
-            case 2:
-                removeElements();
-                pageReserve();
-                break;
-        }
+      switch (idx) {
+        case 0:
+          removeElements();
+          pageAbout();
+          break;
+        case 1:
+          removeElements();
+          pageFood();
+          break;
+        case 2:
+          removeElements();
+          pageReserve();
+          break;
+      }
     });
 }
 
 component();
 
-
-// Make sure to change your devtool in webpack.config.js to something more 
+// Make sure to change your devtool in webpack.config.js to something more
 // production friendly when you finish this project
